4ed75869ef9e2df176f9d7eeab9f528f5e0f8821
michelemarnati <michelemarnati@libero.it>
2021-11-28 14:24:03 +0100

    Michele Marnati - modificato scheletro della schermata principale dell'app.

--- app/src/main/java/it/michelemarnati/moneybook/MainActivity.kt
+++ app/src/main/java/it/michelemarnati/moneybook/MainActivity.kt
@@ -6,45 +6,37 @@ import android.widget.EditText;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import java.util.Properties;
-
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-
-
-
+import java.util.*
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     //Declaring EditText
     private var editTextEmail: EditText? = null
-    private var editTextSubject: EditText? = null
-    private var editTextMessage: EditText? = null
+    private var editTextPsw: EditText? = null
+    private var alphanumeric: String? = null
 
     //Send button
-    private var buttonSend: Button? = null
+    private var loginButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Initializing the views
-        editTextEmail = findViewById<View>(R.id.recipientEt) as EditText
-        editTextSubject = findViewById<View>(R.id.subjectEt) as EditText
-        editTextMessage = findViewById<View>(R.id.messageEt) as EditText
-        buttonSend = findViewById<View>(R.id.sendEmailBtn) as Button
-
+        editTextEmail = findViewById<View>(R.id.editTextMail) as EditText
+        editTextPsw = findViewById<View>(R.id.editTextPassword) as EditText
+        loginButton = findViewById<View>(R.id.loginBtn) as Button
         //Adding click listener
-        buttonSend!!.setOnClickListener(this)
+        loginButton!!.setOnClickListener(this)
     }
 
 
     private fun sendEmail() {
         //Getting content for email
         val email = editTextEmail!!.text.toString().trim { it <= ' ' }
-        val subject = editTextSubject!!.text.toString().trim { it <= ' ' }
-        val message = editTextMessage!!.text.toString().trim { it <= ' ' }
+        val subject = "MoneyBook app authentication"
+        alphanumeric = UUID.randomUUID().toString().substring(0, 8);
+        val message = alphanumeric!!
 
         //Creating SendMail object
         val sm = SendMail(this, email, subject, message)
